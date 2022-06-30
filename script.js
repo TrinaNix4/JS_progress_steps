b4f6dcb5eebedef3145406b45ebb3c4fb6c1de63
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-06-29 20:44:05 -0600

    added event listener for next button

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// bring in from the DOM
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
+// represents whichever one is active; set to 1 by default
+let currentActive = 1;
+
+//take next button, add event listener, when it clicks
+//run a function
+next.addEventListener("click", () => {
+  //take whatever currentActive is at the time and increment by 1
+  currentActive++;
+
+  //don't want it to go past number 4
+  //if currentActive is greater than circles.length (the amount of circles) then set currentActive to whatever the length of circles is
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  console.log(currentActive);
+});
